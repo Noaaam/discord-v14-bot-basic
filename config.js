6c09556dbaf7ac7dda3b75d6c8e72a0a183c8ba5
Noam <68784989+Noaaam@users.noreply.github.com>
2023-03-24 20:06:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Basic configuration
+    token: '', // Place here your token
+    status: '', // Place here the status of the client
+    developerGuildID: '', // Place here your Guild ID
+    database: '', // Place here your MongoDB access
+}
